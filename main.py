ea69f9191b546584da6af64b22d523f6bd353c3f
Yamini Soni <yaminisoni35@gmail.com>
2026-09-04 11:12:01 +0530

    Point distance calculation commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from point import Point
+
+p1 = Point(10, 10)
+p2 = Point(20, 20)
+
+print(p1.distance(p2))
+
---
+++ point.py
@@ -0,0 +1,15 @@
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def distance(self, p):
+        print(p)
+
+        dx = p.x - self.x
+        dy = p.y - self.y
+
+        return (dx ** 2 + dy ** 2) ** 0.5
+
